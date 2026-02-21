7411e396f6d4757aab52f74b73466f20dbf032c8
kurtpaolo <kurtpaoloredondo14@gmail.com>
2026-02-21 12:34:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace BillsPayment
+{
+    class RedondoBillsPayment
+    {
+        static void Main(string[] args)
+        {
+            string recipient, paymentConfirmation = "", continued = "n";
+            double amount;
+            int reference = 1000000;
+            DateTime dateToday = DateTime.Now;
+
+                Console.Write("Would you like to make a payment (y/n): ");
+                continued = Console.ReadLine();
+
+            if (continued == "y")
+            {
+                {
+                    Console.Write("Recipient: ");
+                    recipient = Console.ReadLine();
+                    Console.Write("Amount: ");
+                    amount = double.Parse(Console.ReadLine());
+
+                    Console.Write($"You are about to pay {recipient} an amount of {amount}. Do you want to proceed? (y/n): ");
+                    paymentConfirmation = Console.ReadLine();
+
+                    switch (paymentConfirmation)
+                    {
+                        case "y":
+                            Console.WriteLine($"Your payment of PHP {amount} to");
+                            Console.WriteLine($"{recipient} has been successfully");
+                            Console.WriteLine($"processed on {dateToday}.");
+                            Console.WriteLine($"Reference Number: {reference++}");
+                            break;
+
+                        case "n":
+                            Console.WriteLine("Payment cancelled.");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input. Payment cancelled.");
+                            break;
+                    }
+
+                }
+            }
+            
+            else 
+                Console.WriteLine("Goodbye!");
+        }
+    }
+}
